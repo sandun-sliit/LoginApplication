22b91e4f8e41042f0fd921562a8c31541be3d839
sandun-sliit <sandunharshana19@gmail.com>
2022-01-22 03:13:30 +0530

    Initial commit

---
+++ LoginApplication.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using LoginApplication;
+using LoginApplication.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace LoginApplication.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ LoginApplication/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LoginApplication.Models;
+using LoginApplication.VM;
+
+namespace LoginApplication.Controllers
+{
+    [RoutePrefix("Api/login")]
+    public class LoginController : ApiController
+    {
+        EmployeesEntities DB = new EmployeesEntities();
+        [Route("InsertEmployee")]
+        [HttpPost]
+        public object InsertEmployee(Register Reg)
+        {
+            try
+            {
+                EmployeeLogin EL = new EmployeeLogin();
+                if (EL.Id == 0)
+                {
+                    EL.EmployeeName = Reg.EmployeeName;
+                    //EL.City = Reg.City;
+                    EL.Email = Reg.Email;
+                    EL.Password = Reg.Password;
+                    EL.Username = Reg.Username;
+                    DB.EmployeeLogins.Add(EL);
+                    DB.SaveChanges();
+                    return new Response
+                    { Status = "Success", Message = "Record SuccessFully Saved." };
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return new Response
+            { Status = "Error", Message = "Invalid Data." };
+        }
+        [Route("Login")]
+        [HttpPost]
+        public Response employeeLogin(Login login)
+        {
+            var log = DB.EmployeeLogins.Where(x => x.Email.Equals(login.Email) &&
+                      x.Password.Equals(login.Password)).FirstOrDefault();
+
+            if (log == null)
+            {
+                return new Response { Status = "Invalid", Message = "Invalid User." };
+            }
+            else
+                return new Response { Status = "Success", Message = "Login Successfully" };
+        }
+    }
+}
